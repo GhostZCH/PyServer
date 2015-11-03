3b26cc1222ecb1e9eb3be0db420fdbe25a3e814b
ghost <zhao_chenhui_scu@163.com>
2015-11-03 23:46:03 +0800

    timer

--- bin/svr_timer.py
+++ bin/svr_timer.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+
 import time
 import datetime
+import traceback
+
 from threading import Event
 from threading import Thread
 
@@ -35,30 +38,33 @@ class PeriodTimer(_AbstractTimer):
             return self._max_run_count > self._run_count
         return True
 
+
 class FixedPeriodTimer(PeriodTimer):
     """
     设置固定时间的周期性定时器 D H
     """
     _TYPE_DICT = {
-        'D': (24 * 3600, datetime.timedelta(days=1)),
-        'H': (3600, datetime.timedelta(hours=1))
+        'D': (24 * 3600, datetime.timedelta(days=1), '%Y%m%d'),
+        'H': (3600, datetime.timedelta(hours=1), '%Y%m%d%H')
     }
 
     def __init__(self, target, fix_type, max_run_count=None):
         if fix_type not in FixedPeriodTimer._TYPE_DICT:
             raise Exception('unknown FixedPeriodTimer type, should be "D"(per day) or "H"(per hour)')
         PeriodTimer.__init__(self, target, FixedPeriodTimer._TYPE_DICT[fix_type][0], max_run_count)
-        next_time = datetime.datetime.now() + FixedPeriodTimer._TYPE_DICT[fix_type][1]
-        self._next_time = next_time.ctime()
+
+        self._next_time = FixedPeriodTimer._get_start_time(fix_type)
 
     @classmethod
     def _get_start_time(cls, fix_type):
-
-        return 0
+        next_time = datetime.datetime.now() + FixedPeriodTimer._TYPE_DICT[fix_type][1]
+        next_time = next_time.strftime(FixedPeriodTimer._TYPE_DICT[fix_type][2])
+        next_time = datetime.datetime.strptime(next_time, FixedPeriodTimer._TYPE_DICT[fix_type][2])
+        return time.mktime(next_time.timetuple())
 
 
 class TimerObserver(Thread):
-    def __init__(self, interval, on_except=None):
+    def __init__(self, min_interval, on_except):
         """
         :param interval: 时间间隔
         :param target: 执行的函数
@@ -68,13 +74,23 @@ class TimerObserver(Thread):
         """
 
         Thread.__init__(self)
-        self._interval = interval
-        self._target = target
-        self._args = args
-        self._kwargs = kwargs
+        self._interval = min_interval
+        self._timer_dict = {}
         self._finish_event = Event()
-        self._max_times = max_times
         self._event_on_except = on_except
+
+    def add_timer(self, key, timer):
+        if key in self._timer_dict:
+            return False
+
+        self._timer_dict[key] = timer
+
+    def remove_timer(self, key):
+        if key not in self._timer_dict:
+            return False
+
+        self._timer_dict.pop(key)
+        return True
 
     def run(self):
         while True:
@@ -82,18 +98,13 @@ class TimerObserver(Thread):
 
             if self._finish_event.is_set():
                 return
-            try:
-                self._target(*self._args, **self._kwargs)
-            except BaseException, ex:
-                if self._event_on_except(ex):
-                    raise
 
-            if self._max_times:
-                self._max_times -= 1
-                if self._max_times <= 0:
-                    if self._event_on_finish:
-                        self._event_on_finish
-                    return
+            time_stamp = time.time()
+            for timer in self._timer_dict.values():
+                try:
+                    timer.run(time_stamp)
+                except Exception as ex:
+                    self._event_on_except(ex, traceback.format_exc())
 
     def stop(self):
         self._finish_event.set()
